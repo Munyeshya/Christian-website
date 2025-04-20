02166be8cc55c306358ac7ffbd79d2067257a866
Munyeshya <obedbenny54@gmail.com>
2025-04-20 22:22:45 +0200

    Web remodel

--- base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from base.views import *
-from django.conf import settings
-from django.conf.urls.static import static
-from .views import index
-
-urlpatterns = [
-    path('', index, name='index'),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
